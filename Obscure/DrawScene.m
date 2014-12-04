1eb708db9c9bb9dd44fd0297885a2e76ae8a00dc
Michelle Wong <michellewong.nma@gmail.com>
2014-12-03 21:48:50 -0800

    Done : drawCombatUI WIP : beingAttackedAnimation

--- Obscure/DrawScene.m
+++ Obscure/DrawScene.m
@@ -78,7 +78,8 @@ int seconds = 0;
     else
         [sound playSoundForever:@"duck_hunt_2"];
     
-    [self setVariableButtons];
+    //make screenshot buttons appear
+    //[self setVariableButtons];
     
 }
 
@@ -157,14 +158,56 @@ int seconds = 0;
 
 -(void)drawCombatUI
 {
-    CGPoint center = CGPointMake((topLeft.x+topRight.x)/2, (topLeft.y + bottomLeft.y)/2);
+    //grid overlay
     SKSpriteNode *overlay = [SKSpriteNode spriteNodeWithImageNamed:@"combatUI-overlayfilter.png"];
-    [overlay setPosition:center];
-    [overlay setSize:CGSizeMake(800, 450)];
+    [overlay setPosition:CGPointMake(screenWidth/2, screenHeight/2)];
+    [overlay setSize:CGSizeMake(screenWidth, screenHeight)];
     [overlay setZPosition:-2];
+    
+    //ui decal
+    SKSpriteNode *decal = [SKSpriteNode spriteNodeWithImageNamed:@"ui-Decal.png"];
+    [decal setPosition:CGPointMake(screenWidth/2, screenHeight/2)];
+    [decal setSize:CGSizeMake(screenWidth, screenHeight)];
+    [decal setZPosition:-1];
+    
+    //pause button
+    SKSpriteNode *pause = [SKSpriteNode spriteNodeWithImageNamed:@"pause-button.png"];
+    float pause_height = pause.size.height*0.6;
+    float pause_width = pause.size.width*0.6;
+    [pause setPosition:CGPointMake(screenWidth-pause_width, screenHeight-pause_height)];
+    [pause setSize:CGSizeMake(pause_width, pause_height)];
+    [pause setZPosition:-1];
+    
+    //target cross hair
+    SKSpriteNode *crosshair = [SKSpriteNode spriteNodeWithImageNamed:@"targetcrosshair.png"];
+        // to be changed to position of monster. this is just to test
+    [crosshair setPosition:CGPointMake(screenWidth/3, screenHeight/3)];
+    [crosshair setSize:CGSizeMake(crosshair.size.width*0.5, crosshair.size.height*0.5)];
+    [crosshair setZPosition:-1];
+    
+    //katana
+    SKSpriteNode *katana = [SKSpriteNode spriteNodeWithImageNamed:@"weapon-katana.png"];
+    [katana setPosition:CGPointMake(screenWidth-175, screenHeight-337)];
+    [katana setSize:CGSizeMake(katana.size.width*0.6, katana.size.height*0.6)];
+    [katana setZPosition:-1];
+
     //[overlay setSize: CGPointMake(100, 100)];
     [self addChild:overlay];
-    
+    [self addChild:decal];
+    [self addChild:pause];
+    [self addChild:crosshair];
+    [self addChild:katana];
+}
+
+-(void)beingAttackedAnimation
+{
+    //warning symbol
+    SKSpriteNode *warning = [SKSpriteNode spriteNodeWithImageNamed:@"Warning-Symbol.png"];
+    [warning setPosition:CGPointMake(screenWidth/2, screenHeight/2)];
+    [warning setSize:CGSizeMake(warning.size.width*0.5, warning.size.height*0.5)];
+    [warning setZPosition:-1];
+    [self addChild:warning];
+
 }
 
 //Make a duck, give it a spritesheet, give it animation, give it sound
