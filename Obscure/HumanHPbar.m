e6ce75695aa7ba37a481a927d806de698b2d6fa4
Michelle Tjoa <michelletjoa@dhcp-v185-065.av.reshsg.uci.edu>
2015-01-14 00:11:48 -0800

    created constant variable for the health
    still need to change a bit to make sure it works correctly

--- Obscure/HumanHPbar.m
+++ Obscure/HumanHPbar.m
@@ -7,19 +7,22 @@
 //
 
 #import "HumanHPbar.h"
+#define TOTAL_HEALTH 100
 
 @implementation HumanHPbar
-- (id)init {
-    if (self = [super init]) {
-        
+- (id)init
+{
+    if (self = [super init])
+    {
         sprite = [SKSpriteNode spriteNodeWithImageNamed:@"health3"];
         CGRect screenRect = [[UIScreen mainScreen] bounds];
         float screenWidth = screenRect.size.width;
         float screenHeight = screenRect.size.height;
         [sprite setPosition:CGPointMake(sprite.size.width, screenHeight-sprite.size.height)];
         NSLog(@"%f,%f", screenWidth, screenHeight);
-        hp = 3;
-        [self addChild:sprite];
+        hp = TOTAL_HEALTH
+//        damage = 50;
+        //[self addChild:sprite];
     }
     return self;
 }
@@ -30,33 +33,45 @@
         if (hp == 1)
         {
             image = [SKTexture textureWithImageNamed:@"health2"];
-        }else if(hp == 2)
+        }
+        
+        else if (hp == 2)
         {
             image = [SKTexture textureWithImageNamed:@"health3"];
-        }else if(hp ==0)
+        }
+        
+        else if (hp ==0)
         {
              image = [SKTexture textureWithImageNamed:@"health1"];
         }
+        
         [sprite setTexture:image];
         hp+=1;
     }
 }
+
+
 - (void) humanwound
 {
-    if (hp >= 1)
+    if (TOTAL_HEALTH >= 1)
     {
-        if (hp == 3)
+        if (TOTAL_HEALTH == 3)
         {
             image = [SKTexture textureWithImageNamed:@"health2"];
-        }else if(hp == 2)
+        }
+        
+        else if(TOTAL_HEALTH == 2)
         {
             image = [SKTexture textureWithImageNamed:@"health1"];
-        }else if(hp ==1)
+        }
+        
+        else if(TOTAL_HEALTH ==1)
         {
             image = [SKTexture textureWithImageNamed:@"health0"];
         }
+        
         [sprite setTexture:image];
-        hp-=1;
+        hp-=damage;
     }
 }
 
@@ -64,4 +79,5 @@
 {
     return hp;
 }
+
 @end
