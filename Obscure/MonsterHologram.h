56d2edc8394442b9fc2f31a3c148a619b7a261c1
Minjae Wee <kaylab@dhcp-v126-142.mobile.uci.edu>
2015-02-03 21:53:30 -0800

    Completed MonsterHologram.m/h

--- Obscure/MonsterHologram.h
+++ Obscure/MonsterHologram.h
@@ -1,13 +1,18 @@
-//
-//  MonsterHologram.h
-//  Obscure
-//
-//  Created by Minjae Wee, Michelle Tjoa, and Michelle Wong on 1/27/15.
-//  Copyright (c) 2015 Calvin Tham. All rights reserved.
-//
+#import <Foundation/Foundation.h>
+#import <SpriteKit/SpriteKit.h>
 
-#ifndef Obscure_MonsterHologram_h
-#define Obscure_MonsterHologram_h
+#define DMG 10
 
+@interface MonsterHologram : NSObject
+{
+    SKSpriteNode* sprite;
+    float health;
+}
 
-#endif
+@property SKSpriteNode *sprite;
+-(id)init :(float)x :(float)y :(float)width :(float)height;
+-(void)hurt;
+-(void)update;
+-(void)checkForTouchesBegan:(CGPoint)loc;
+
+@end
