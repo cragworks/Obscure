5cd7cdb321909c228d7bc95c6ec0cda91207163a
Michelle Tjoa <michelletjoa@dhcp-v126-063.mobile.uci.edu>
2015-02-10 20:52:55 -0800

    put back hologram
    return lost file

--- Obscure/MonsterHologram.m
+++ Obscure/MonsterHologram.m
@@ -1,24 +1,34 @@
-//
-//  MonsterHologram.m
-//  Obscure
-//
-//  Created by Minjae Wee, Michelle Tjoa, and Michelle Wong on 1/27/15.
-//  Copyright (c) 2015 Calvin Tham. All rights reserved.
-//
+#import "MonsterHologram.h"
 
-#import <Foundation/Foundation.h>
+@implementation MonsterHologram
+@synthesize sprite;
 
-// monsterHologram
+-(id)init :(float)x :(float)y :(float)width :(float)height
+{
+    self = [super init];
+    [sprite setPosition:CGPointMake(x, y)];
+    [sprite setSize:CGSizeMake(width, height)];
+    return self;
+}
 
-// <Variables>
-//  SKSpriteNode * sprite;
-//  float health;
+-(void)hurt
+{
+    health -= DMG;
+}
 
-// <Functions>
+-(void)update
+{
+//Might need to add sth later.
+}
+
+-(void)checkForTouchesBegan:(CGPoint)loc
+{
 /*
-    init(int x, int y, int width, int height);
-    hurt(); // called when hurt
-
-    update(); // Update necessary things if needed like animations
-    checkForTouchesBegan(); // Check if touchedBegan, then do necessary tasks
+    if ([[self nodeAtPoint:loc].name isEqualToString:MonsterHologram.name])
+    {
+        hurt();
+    }
 */
+}
+    
+@end
